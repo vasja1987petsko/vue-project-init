18e1e4a3371e86e831b37fd891ce154653276567
Vasyl Petsko <petsko.vi@gmail.com>
2018-10-30 10:59:30 +0200

    API_URL axios

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,7 @@
 import axios from 'axios';
+//Settings for axios
+
+axios.defaults.baseURL = `${process.env.API_URL}`;
 
 axios.interceptors.response.use((response)=> {
     // Do something with response data
@@ -9,10 +12,13 @@ axios.interceptors.response.use((response)=> {
     //console.log('interceptors',error);
     return Promise.reject(error);
 });
+//End settings for axios
 
 function api(config) {
 
     return new Promise(function(resolve, reject) {
+        //To do  formatHttpHeaders
+
         return axios(config).then((response)=>{
             resolve(response) ;
         }).catch((error)=>{
