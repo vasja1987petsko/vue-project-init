8f49ffc128002c0b6db4813517856ba9efc661f0
Vasyl Petsko <petsko.vi@gmail.com>
2018-10-30 18:08:40 +0200

    bootstrap

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css' //if don't use  in main.scss bootstrap.css
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from './App'
 import router from './router'
 import store from './store'
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false
 
