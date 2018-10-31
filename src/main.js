50a29735f94cf8da1bfe382edb357756f9229e22
Vasyl Petsko <petsko.vi@gmail.com>
2018-10-31 11:58:05 +0200

    bootstrap from npm + variables from bootstrap

--- src/main.js
+++ src/main.js
@@ -2,15 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css' //if don't use  in main.scss bootstrap.css
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+//import BootstrapVue from 'bootstrap-vue'//if need BootstrapVue
+//import 'bootstrap/dist/css/bootstrap.css' //if don't use  in main.scss bootstrap.css
+//import 'bootstrap-vue/dist/bootstrap-vue.css' //if need BootstrapVue
 
 import App from './App'
 import router from './router'
 import store from './store'
 
-Vue.use(BootstrapVue);
+//Vue.use(BootstrapVue);//if need BootstrapVue
 
 Vue.config.productionTip = false
 
